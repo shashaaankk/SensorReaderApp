113828d6cca358b0e0513d03ab896b7d82ee7b72
Shashaaankk <shashaankbm@gmail.com>
2024-04-19 18:55:32 +0200

    "Refined Logic"

--- app/src/main/java/com/example/sensorreader/SecondActivity.java
+++ app/src/main/java/com/example/sensorreader/SecondActivity.java
@@ -1,6 +1,9 @@
 package com.example.sensorreader;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -19,18 +22,15 @@ import android.hardware.lights.Light;
 import android.hardware.GeomagneticField.*;
 import android.widget.Toast;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity{
 
     private TextView display_sensVal, display_period, display_threshold;   // TextView to display the sensor Values
-    private SeekBar seekBarThreshold;   // SeekBar for adjusting the threshold
     private SeekBar seekBarPeriod;
     private Button buttonpreviousActivity;
     private SensorManager sensorManager;
-    private Sensor lightSensor;
     private Sensor accelerometer;
-    private int threshold = 1; // Default threshold
-
-    public static final int TYPE_LIGHT =0;
+    final private double threshold = 5.0; // Default threshold
+    private SensorEventListener sensorEventListenerAccelerometer;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -41,47 +41,31 @@ public class SecondActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
             setupSensors();
             return insets;
         });
 
         display_period  = (TextView) findViewById(R.id.textView_period);
+        seekBarPeriod = (SeekBar) findViewById(R.id.seekBar_period);
+
         display_threshold  = (TextView) findViewById(R.id.textView_threshold);
+        display_threshold.setText("Threshold: " + threshold + " m/s^2");
+
         display_sensVal  = (TextView) findViewById(R.id.textView_sensorVal);
 
-        seekBarThreshold = (SeekBar) findViewById(R.id.seekBar_threshold);
-        seekBarPeriod = (SeekBar) findViewById(R.id.seekBar_period);
         buttonpreviousActivity = (Button) findViewById(R.id.second_activity_button);
 
-        // for the threshold
-        seekBarThreshold.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                display_threshold.setText("Threshold: " + progress + " T");
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                //functionality when the user starts touching the SeekBar
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                //functionality when the user stops touching the SeekBar
-            }
-        });
         // for period
         seekBarPeriod.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 display_period.setText("Period: " + progress + "s");
             }
-
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
                 //functionality when the user starts touching the SeekBar
             }
-
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 //functionality when the user stops touching the SeekBar
@@ -96,35 +80,36 @@ public class SecondActivity extends AppCompatActivity {
             startActivity(intent);
         });
     }
+
     private void setupSensors() {
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        //sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        if(accelerometer == null){
+        if (accelerometer == null) {
             Toast.makeText(this, "Device has no light sensor!", Toast.LENGTH_SHORT).show();
         }
 
-        SensorEventListener sensorEventListenerAccelerometer = new SensorEventListener() {
+        SensorEventListener sensorEventListenerLight = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
-                if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
-                {
-                    display_sensVal.setText("X:  " + event.values[0] + "| Y:  "+event.values[1] + "| Z:  "+event.values[2] +"m/s^2");
+                float roundedx = Math.round(event.values[0] * 100.0f) / 100.0f;
+                float roundedy = Math.round(event.values[1] * 100.0f) / 100.0f;
+                float roundedz = Math.round(event.values[2] * 100.0f) / 100.0f;
+                if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                    display_sensVal.setText("X:  " + roundedx + "| Y:  " + roundedy + "| Z:  " + roundedz + " m/s^2");
+                    if (Math.abs(roundedz) > threshold) {
+                        Intent intent = new Intent("com.readsensorapp.SENSOR_THRESHOLD");
+                        intent.putExtra("z_value", roundedz);
+                        sendBroadcast(intent);
+                    }
                 }
-
             }
 
             @Override
             public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+                //Not Used
             }
         };
-        sensorManager.registerListener(sensorEventListenerAccelerometer, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(sensorEventListenerLight, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (lightSensor != null) {
-            sensorManager.registerListener((SensorEventListener) this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL);
-        }
-    }
+
 }
